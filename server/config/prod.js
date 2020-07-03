4ed72eaec043445c04d5017a30849b755c47b7f0
Soo-ss <kms334488@naver.com>
2020-07-04 03:20:03 +0900

    key

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,12 @@
 module.exports = {
   mongoURI: process.env.MONGO_URL,
+  AWS_KEY: process.env.AWS_KEY,
+  AWS_PRIVATE_KEY: process.env.AWS_PRIVATE_KEY,
+  COOL_API_KEY: process.env.COOL_API_KEY,
+  COOL_API_SECRET: process.env.COOL_API_SECRET,
+  COOL_MY_PHONE: process.env.COOL_MY_PHONE,
+  NAVER_ID: process.env.NAVER_ID,
+  NAVER_SECRET: process.env.NAVER_SECRET,
+  COOKIE_SECRET: process.env.COOKIE_SECRET,
+  ADMIN_EMAIL_SECRET: process.env.ADMIN_EMAIL_SECRET,
 };
